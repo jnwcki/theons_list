c2b2da0a27d84d6649fb1e27fe4ceb6ff86f5e89
jnwcki <jude.nowicki@gmail.com>
2016-03-09 15:58:53 -0500

    created schema

--- theonsapp/models.py
+++ theonsapp/models.py
@@ -1,3 +1,34 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
+
+class City(models.Model):
+    city_name = models.CharField(max_length=50)
+
+
+class UserProfile(models.Model):
+    user = models.OneToOneField("auth.User")
+    home_city = models.ForeignKey(City, null=True)
+
+
+class Item(models.Model):
+    location = models.ForeignKey(City)
+    seller = models.ForeignKey("auth.User", related_name="item_for_sale")
+    name = models.CharField(max_length=100)
+    full_description = models.TextField()
+    time_listed = models.DateTimeField(auto_now_add=True)
+    photo = models.ImageField()
+
+
+@receiver(post_save, sender="auth.User")
+def user_profile_create(sender, **kwargs):
+    created = kwargs.get("created")
+    if created:
+        instance = kwargs.get("instance")
+        UserProfile.objects.create(user=instance)
+
+
--- theonsapp/views.py
+++ theonsapp/views.py
@@ -1,9 +1,26 @@
+from django.contrib.auth.models import User
+from django.core.urlresolvers import reverse
 from django.shortcuts import render
 
 # Create your views here.
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, CreateView
+from theonsapp.forms import NewUserCreation
 
 
 class IndexView(TemplateView):
     template_name = 'index.html'
-    pass
+
+
+class UserCreateView(CreateView):
+    model = User
+    form_class = NewUserCreation
+
+    def form_valid(self, form):
+        new_user = form.save()
+        new_user_screen_name = form.cleaned_data.get('screen_name')
+        new_user.userprofile.screen_name = new_user_screen_name
+        new_user.userprofile.save()
+        return super().form_valid(form)
+
+    def get_success_url(self):
+        return reverse('login')
