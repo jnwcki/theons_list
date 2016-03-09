020b2569cd256b267288bd69de9aea4015575e3d
jnwcki <jude.nowicki@gmail.com>
2016-03-09 16:51:55 -0500

    loaded test data into models

--- theonsapp/admin.py
+++ theonsapp/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from theonsapp.models import UserProfile, Item, City
 
-# Register your models here.
+admin.site.register(UserProfile)
+admin.site.register(Item)
+admin.site.register(City)
--- theonsapp/models.py
+++ theonsapp/models.py
@@ -1,7 +1,5 @@
 from django.contrib.auth.models import User
 from django.db import models
-
-# Create your models here.
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
@@ -9,10 +7,16 @@ from django.dispatch import receiver
 class City(models.Model):
     city_name = models.CharField(max_length=50)
 
+    def __str__(self):
+        return self.city_name
+
 
 class UserProfile(models.Model):
     user = models.OneToOneField("auth.User")
     home_city = models.ForeignKey(City, null=True)
+
+    def __str__(self):
+        return self.user.username
 
 
 class Item(models.Model):
@@ -21,7 +25,11 @@ class Item(models.Model):
     name = models.CharField(max_length=100)
     full_description = models.TextField()
     time_listed = models.DateTimeField(auto_now_add=True)
-    photo = models.ImageField()
+    photo = models.ImageField(blank=True)
+    price = models.IntegerField()
+
+    def __str__(self):
+        return self.name
 
 
 @receiver(post_save, sender="auth.User")
