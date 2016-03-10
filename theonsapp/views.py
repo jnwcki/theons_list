419fa06ec5a866ea5ddfcb09a1ee49cd79bc72b8
jnwcki <jude.nowicki@gmail.com>
2016-03-10 16:52:10 -0500

    working item ordering

--- theonsapp/views.py
+++ theonsapp/views.py
@@ -1,26 +1,20 @@
 from django.contrib.auth.models import User
 from django.core.urlresolvers import reverse
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
-from django.views.generic import TemplateView, CreateView, ListView
+from django.views.generic import TemplateView, CreateView, ListView, DetailView
 from theonsapp.forms import NewUserCreation
-from theonsapp.models import Item, SubCategory, Category
+from theonsapp.models import Item, SubCategory, Category, City, UserProfile
 
 
 class IndexView(ListView):
     template_name = 'index.html'
     model = Category
 
+
 class UserCreateView(CreateView):
     model = User
     form_class = NewUserCreation
-
-    #def form_valid(self, form):
-        #new_user = form.save()
-        #new_user_screen_name = form.cleaned_data.get('screen_name')
-        #new_user.userprofile.screen_name = new_user_screen_name
-        #new_user.userprofile.save()
-        #return super().form_valid(form)
-        #pass
 
     def get_success_url(self):
         return reverse('login')
@@ -28,18 +22,54 @@ class UserCreateView(CreateView):
 
 class MakeListingView(CreateView):
     model = Item
-    fields = ['name', 'full_description']
+    fields = ['name', 'full_description', 'photo', 'price']
 
     def form_valid(self, form):
         post = form.save(commit=False)
-        post.sub_category = SubCategory.objects.get(pk=self.kwargs.get(subcategory_id))
+        post.subcategory = SubCategory.objects.get(pk=self.kwargs.get('pk'))
+
+        user_profile = UserProfile.objects.get(user=self.request.user)
+        post.location = City.objects.get(pk=user_profile.home_city.pk)
+        post.seller_id = user_profile.user.pk
+        print(user_profile)
+        post.save()
+        return super().form_valid(form)
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['subcategory'] = SubCategory.objects.get(pk=self.kwargs.get(subcategory_id))
+        context['subcategory'] = SubCategory.objects.get(pk=self.kwargs.get('pk'))
+        return context
+
+    def get_success_url(self):
+        return reverse('index')
+
+
+class CategoryDetailView(DetailView):
+    model = Category
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['category'] = Category.objects.get(pk=self.kwargs.get('pk'))
         return context
 
 
-class CategoryListView(ListView):
-    model = Category
+class SubCategoryItemView(ListView):
+    model = Item
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['subcategory'] = SubCategory.objects.get(pk=self.kwargs.get('pk'))
+        #print(ordering)
+        print(self.kwargs.get('ordering', 1))
+        if self.kwargs.get('ordering') == '1':
+            context['item_list'] = Item.objects.filter(subcategory_id=self.kwargs.get('pk')).order_by("-time_listed")
+        elif self.kwargs.get('ordering') == '2':
+            context['item_list'] = Item.objects.filter(subcategory_id=self.kwargs.get('pk')).order_by("-price")
+        elif self.kwargs.get('ordering') == '3':
+            context['item_list'] = Item.objects.filter(subcategory_id=self.kwargs.get('pk')).order_by("price")
+        print(context)
+        return context
+
+
+class ItemDetailView(DetailView):
+    model = Item
